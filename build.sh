524ea8e623ec54bc7f1d30be367ac3fc932136ba
Louis Harris <ruisuharisu@gmail.com>
2025-04-14 10:20:03 -0400

    Update to current versioning

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ if [ "$PROFILE" == "java-21" ]; then
         --type exe \
         --dest "equipmentforge-installer" \
         --icon "target/classes/assets/images/equipmentforge/equipmentforge-icon.ico" \
-        --app-version "1.2.3" \
+        --app-version "1.2.4" \
         --win-shortcut \
         --win-menu \
         --description "Data Solutions Made Easier" \
