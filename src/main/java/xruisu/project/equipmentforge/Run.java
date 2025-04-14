36b504d09a0b5cbfa32bb1a767af0eda25b0775d
Louis Harris <ruisuharisu@gmail.com>
2025-04-14 06:29:57 -0400

    Update 1.2.4 beta

---
+++ src/main/java/xruisu/project/equipmentforge/Run.java
@@ -0,0 +1,9 @@
+package xruisu.project.equipmentforge;
+
+public class Run {
+
+    public static void main(String[] args) {
+        System.out.println("\n...Running... " + " \n\n");
+        App.main(args);
+    }
+}
